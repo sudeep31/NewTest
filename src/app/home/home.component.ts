be75a96d6dc916e2abf484a06d50d7ea1459d4bf
sudeep31 <sudeep.parchure@gmail.com>
2022-07-24 19:40:23 +0530

    adding error messages

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,6 +12,7 @@ export class HomeComponent implements OnInit {
   @Input() item = { name: "" };
   @Output() newItemEvent = new EventEmitter();
   holdGitImages: any = [];
+  displayError: any;
   constructor(public getGifImageService: GetGifImagesService) { }
 
   ngOnInit(): void {
@@ -21,11 +22,12 @@ export class HomeComponent implements OnInit {
   findGif() {
 
     let getValue = <HTMLInputElement>document.getElementById("search");
-
+    this.displayError = ""
     if (getValue && getValue.value != '' && getValue.value != undefined) {
       this.DisplayGif(getValue);
+
     } else {
-      alert("please fill the data")
+      this.displayError = "please fill the data"
     }
 
 
@@ -35,6 +37,7 @@ export class HomeComponent implements OnInit {
   }
 
   DisplayGif(data: any) {
+    this.displayError = "No records found"
     this.getGifImageService.getGifImageService(data).subscribe((res: any) => {
       let data = [];
       if (res && res.data.length > 1)
